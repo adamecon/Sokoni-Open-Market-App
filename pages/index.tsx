afc02187e82944c0685275c4b84633933743f9ba
adamecon <67404908+adamecon@users.noreply.github.com>
2022-09-16 16:18:29 +0300

    product cards added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import type { NextPage } from "next";
 import Layout from "../components/Layout";
+import ProductCard from "../components/Productcard";
+import data from "../utils/data";
 
 const Home: NextPage = () => {
   return (
     <Layout title="Sokoni">
-      <h1>Welcome Home</h1>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductCard product={product} key={product.slug} />
+        ))}
+      </div>
     </Layout>
   );
 };
