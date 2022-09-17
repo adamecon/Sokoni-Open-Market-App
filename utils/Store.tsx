a37ae8a545c7c0e7218cb34061321a89ad1ef1e5
adamecon <67404908+adamecon@users.noreply.github.com>
2022-09-17 17:30:44 +0300

    store functionality

---
+++ utils/Store.tsx
@@ -0,0 +1,29 @@
+import { createContext, useReducer } from "react";
+
+export const Store = createContext();
+
+const initialState = {
+  cart: { cartItems: [] },
+};
+
+function reducer(state: any, action: any) {
+  switch (action.type) {
+    case "ADD_CART_ITEMS": {
+      const newItem = action.payload;
+      const existItem = state.cart.cartItems.find(
+        (item: any) => item.slug === newItem.slug
+      );
+      const cartItems = existItem
+        ? state.cart.cartItems.map((item: any) =>
+            item.name === existItem.name ? newItem : item
+          )
+        : [...state.cart.cartItems, newItem];
+      return { ...state, cart: { ...state.cart, cartItems } };
+    }
+    default:
+      return state;
+  }
+}
+export function StoreProvider({ children }: any) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+}
