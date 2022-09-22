35ea2029861467b6a5232ecdd41af8e41c52c8e4
adamecon <67404908+adamecon@users.noreply.github.com>
2022-09-22 09:43:02 +0300

    some fixes

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -4,12 +4,22 @@ import { Store } from "../utils/Store";
 import Link from "next/link";
 import Image from "next/image";
 import { XCircleIcon } from "@heroicons/react/outline";
+import { Router, useRouter } from "next/router";
 
 const CartScreen = () => {
+  const router = useRouter();
   const { state, dispatch }: any = useContext(Store);
   const {
     cart: { cartItems },
   } = state;
+
+  const removeItemHandler = (item: any) => {
+    dispatch({ type: "CART_REMOVE_ITEM", payload: item });
+  };
+  const updateCartHandler = (item: any, qty: any) => {
+    const quantity = Number(qty);
+    dispatch({ type: "CART_ADD_ITEM", payload: { ...item, quantity } });
+  };
   return (
     <Layout title="Your Shopping Cart">
       <h1 className="mb-4 text-xl font-bold">Your Shopping Cart</h1>
@@ -30,32 +40,74 @@ const CartScreen = () => {
                   <th className="p-5">Action</th>
                 </tr>
               </thead>
+
+              <tbody>
+                {cartItems.map((item: any) => (
+                  <tr key={item.slug} className="border-b">
+                    <td>
+                      <Link href={`/product/${item.slug}`}>
+                        <a className="flex items-center">
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            width={50}
+                            height={50}
+                          ></Image>
+                          &nbsp;
+                          {item.name}
+                        </a>
+                      </Link>
+                    </td>
+                    <td className="p-5 text-right">
+                      <select
+                        value={item.quantity}
+                        onChange={(e) =>
+                          updateCartHandler(item, e.target.value)
+                        }
+                      >
+                        {[...Array(item.stock).keys()].map((x: any) => (
+                          <option key={x + 1} value={x + 1}>
+                            {x + 1}
+                          </option>
+                        ))}
+                      </select>
+                    </td>
+                    <td className="p-5 text-right">${item.price}</td>
+                    <td className="p-5 text-center">
+                      <button
+                        className="primary-button"
+                        onClick={() => removeItemHandler}
+                      >
+                        Remove
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
             </table>
-            <tbody>
-              {cartItems.map((item: any) => (
-                <tr key={item.slug} className="border-b">
-                  <td>
-                    <Link href={`/product/${item.slug}`}>
-                      <a className="flex items-center">
-                        <Image
-                          src={item.image}
-                          alt={item.name}
-                          width={50}
-                          height={50}
-                        ></Image>
-                        &nbsp;
-                        {item.name}
-                      </a>
-                    </Link>
-                  </td>
-                  <td className="p-5 text-right">{item.quantity}</td>
-                  <td className="p-5 text-right">${item.price}</td>
-                  <td className="p-5 text-center">
-                    <button className="primary-button">Remove</button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
+          </div>
+          <div className="card p-5">
+            <ul>
+              <li>
+                <div className="pb-3 text-xl">
+                  Total (
+                  {cartItems.reduce((a: any, c: any) => a + c.quantity, 0)}){""}
+                  : $
+                  {cartItems.reduce(
+                    (a: any, c: any) => a + c.quantity * c.price,
+                    0
+                  )}
+                </div>
+              </li>
+              <li>
+                <button
+                  className="primary-button w-full"
+                  onClick={() => router.push("/shiping")}
+                >
+                  Proceed To Check Out
+                </button>
+              </li>
+            </ul>
           </div>
         </div>
       )}
