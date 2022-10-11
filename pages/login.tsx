95191ddaeb9148a35b8b8bf010cb895ecc07ddfc
adamecon <67404908+adamecon@users.noreply.github.com>
2022-10-11 21:02:11 +0300

    a commit

--- pages/login.tsx
+++ pages/login.tsx
@@ -5,7 +5,7 @@ import { useForm, Resolver } from "react-hook-form";
 
 export default function LoginScreen() {
   const {
-    handlesubmit,
+    handleSubmit,
     register,
     formState: { errors },
   } = useForm();
@@ -14,15 +14,48 @@ export default function LoginScreen() {
 
   return (
     <Layout title="Login">
-      <form className="mx-auto max-w-screen" onSubmit={handlesubmit}>
+      <form
+        className="mx-auto max-w-screen"
+        onSubmit={handleSubmit(submitHandler)}
+      >
         <h1 className="mb-4 text-xl">Log In</h1>
         <div className="mb-4">
           <label htmlFor="email"> Email</label>
-          <input type="email" className="w-full" id="email" autoFocus />
+          <input
+            type="email"
+            {...register("email", {
+              required: "Enter Your Email!",
+              pattern: {
+                value: /^[a-zA-z0-9_.+-]+@[a-zA-z0-9-]+[a-zA-z0-9-.]+$/i,
+                message: "Enter a valid email",
+              },
+            })}
+            className="w-full"
+            id="email"
+            autoFocus
+          />
+          {errors.email && (
+            <div className="text-red-500">{errors.email.message}</div>
+          )}
         </div>
         <div className="mb-4">
           <label htmlFor="password">Password</label>
-          <input type="text" className="w-full" id="password" autoFocus />
+          <input
+            type="password"
+            {...register("password", {
+              required: "Enter your password",
+              minLength: {
+                value: 8,
+                message: "password cannot be less than 8 characters",
+              },
+            })}
+            className="w-full"
+            id="password"
+            autoFocus
+          />
+          {errors.password && (
+            <div className="text-red-500">{errors.password.message}</div>
+          )}
         </div>
         <div className="mb-4">
           <button className="primary-button">Log In</button>
